90e7ee826af9de1d1064314549cf3dea54eea8ed
Grisalle <grommp007@gmail.com>
2023-11-05 21:06:37 +0200

    sum

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,6 +38,21 @@ export class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <section className="section">
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -44,7 +44,7 @@ export class ContactForm extends Component {
             value={this.state.name}
             onChange={this.handleChange('name')}
             name="name"
-            maxlength="16"
+            maxLength="16"
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -58,7 +58,7 @@ export class ContactForm extends Component {
             value={this.state.number}
             onChange={this.handleChange('number')}
             name="number"
-            maxlength="13"
+            maxLength="13"
             pattern="\+?\d{1,4}?[.\-\s]?\(?\d{1,3}?\)?[.\-\s]?\d{1,4}[.\-\s]?\d{1,4}[.\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
